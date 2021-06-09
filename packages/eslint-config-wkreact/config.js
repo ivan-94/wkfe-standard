b706e4b071bc9893dc30516910eaf7b8ad447f36
黎贵奇 <carney520@hotmail.com>
2021-06-09 16:11:02 +0800

    feat: 升级 react eslint 配置
    Change-Id: I2a75c70fd900aea7fbcc0da542518c4ad2f48675

--- packages/eslint-config-wkreact/config.js
+++ packages/eslint-config-wkreact/config.js
@@ -1,30 +1,14 @@
 exports.createConfig = function createConfig(loose = false) {
   return {
-    extends: ['standard-react', 'plugin:react-hooks/recommended', 'prettier', 'prettier/react'],
+    extends: ['react-app', 'prettier'],
     plugins: [],
     rules: {
       ...exports.rules,
       ...(loose ? exports.looseRules : {}),
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
   };
 };
 
-exports.rules = {
-  'react/self-closing-comp': 'off',
-  'react/prop-types': 'off',
-  'react/style-prop-object': 'error',
-  'react/jsx-boolean-value': 'warn',
-  'react/no-unsafe': 'error',
-  'react/no-deprecated': 'error',
-  'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.tsx'] }],
-};
+exports.rules = {};
 
-exports.looseRules = {
-  'react/no-unsafe': 'warn',
-  'react/no-deprecated': 'warn',
-};
+exports.looseRules = {};
